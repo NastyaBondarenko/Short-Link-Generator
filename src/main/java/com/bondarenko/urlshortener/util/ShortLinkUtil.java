4d933b6e19e42173dba95ae064c3dfa586551ff7
NastyaBondarenko <av_bondarenko@rambler.ru>
2024-03-01 12:33:22 +0200

    Refactor DefaultShortLinkGenerator
    - renamed variables in  DefaultShortLinkGenerator

--- src/main/java/com/bondarenko/urlshortener/util/ShortLinkUtil.java
+++ src/main/java/com/bondarenko/urlshortener/util/ShortLinkUtil.java
@@ -5,12 +5,12 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ShortLinkUtil {
 
-    public static final String NUMBERS = "0123456789";
-    public static final String LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    public static final String LINK_NUMBERS = "0123456789";
+    public static final String LINK_LETTERS = "abcdefghijklmnopqrstuvwxyz";
     public static final int LINK_LIMIT_LOWER = 0;
     public static final int LINK_LIMIT_UPPER = 4;
 
     public static String getSymbolsForShortLinkGeneration() {
-        return String.join("", NUMBERS, LETTERS, LETTERS.toUpperCase());
+        return String.join("", LINK_NUMBERS, LINK_LETTERS, LINK_LETTERS.toUpperCase());
     }
 }
